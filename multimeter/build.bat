7661c3af6e628292fd722ae5767985de9a21b46e
hsilomedus <pancec@yahoo.com>
2013-09-04 16:06:40 +0200

    Integrated pi4j and Java-WebSocket

--- multimeter/build.bat
+++ multimeter/build.bat
@@ -1,23 +1,41 @@
 @echo off
+
 echo ----------------------------------------------------------------------
 echo Building pi4jmultimeter 
+
 echo Clean before
 rmdir /S /Q mantarget
+
 echo Create directory
 mkdir mantarget
+
 echo Copy manifest
 copy manifest.mf mantarget\
+
 echo Compile
-javac -d mantarget -cp libs\* src\main\java\mk\hsilomedus\multimeter\*.java
+REM javac -d mantarget -cp libs\* src\main\java\mk\hsilomedus\multimeter\*.java
+dir /s /B *.java > sources.txt
+javac -d mantarget -cp libs\* @sources.txt
+del sources.txt
+
 cd mantarget
-echo Copy libs
-copy ..\libs\*.jar .\
+
+REM echo Copy libs
+REM copy ..\libs\Java-WebSocket-1.3.0.jar .\
+
 echo Package
-jar -cfm pi4jmultimeter.jar manifest.mf mk\hsilomedus\multimeter\*.class
+dir /s /B *.class > sources.txt
+jar -cfm pi4jmultimeter.jar manifest.mf mk/* com/* org/*
+del sources.txt
+
 echo Clean after
 rmdir /S /Q mk
+rmdir /S /Q com
+rmdir /S /Q org
 del manifest.mf
+
 cd ..
+
 echo Finished!
 echo ----------------------------------------------------------------------
 @echo on
