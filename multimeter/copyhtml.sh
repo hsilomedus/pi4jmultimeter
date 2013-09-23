556808653bec2d8150047aafb93f02aff22cd3f7
pcavkovs <pcavkovs@NB5089.netcetera.mk>
2013-09-23 21:46:29 +0200

    copy html script alter

--- multimeter/copyhtml.sh
+++ multimeter/copyhtml.sh
@@ -1,3 +1,3 @@
 sudo cp src/main/html/*.html /var/www/
-sudo cp -r src/main/js /var/www/
-sudo cp -r src/main/css /var/www/
+sudo cp -r src/main/html/js /var/www/
+sudo cp -r src/main//html/css /var/www/
